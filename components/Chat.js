1e825dcc70b691124aa06f2f1573ea1e84e22fce
MC <maciej.p@emiter.net.pl>
2025-02-03 17:37:14 +0100

    add Chat component add server side socket

---
+++ components/Chat.js
@@ -0,0 +1,56 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+let socket = io()
+
+// socket = io();
+export default function Chat() {
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+
+        socket.on("setDone", (elem) => {
+            setMessages(prevState => prevState.map((item, index) => index == elem.itemIndex ? Object.assign({}, item, { value: !elem.value }) : item))
+        });
+
+        socket.on("addMessage", (elem) => {
+            setMessages((prevState) => [...prevState, { name: elem.name, value: elem.value }]);
+        });
+    }, []);
+
+    const testFunc = (itemValue, itemIndex) => {
+        setMessages(prevState => prevState.map((item, index) => index == itemIndex ? Object.assign({}, item, { value: !itemValue }) : item))
+        socket.emit("setDone", { value: itemValue, itemIndex });
+    }
+
+    const sendMessage = () => {
+        setMessages((prevState) => [...prevState, { name: message, value: false }])
+        socket.emit('addMessage', { name: message, value: false })
+        setMessage('')
+    }
+
+    return (
+        <div className="flex flex-col">
+            <h1>Real-Time Chat</h1>
+
+            <input value={message} onChange={(e) => setMessage(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+
+            <button onClick={sendMessage} className="w-fit">Send</button>
+
+            {
+                messages.map((item, index) => {
+                    return (
+                        <label key={index} className="checkbox-container">
+                            {item.name}
+                            <input className="custom-checkbox" onChange={() => testFunc(item.value, index)} checked={item.value} type="checkbox" />
+                            <span className="checkmark"></span>
+                        </label>
+                    )
+                }).sort((a, b) => a.index - b.index)
+            }
+        </div>
+    );
+}
---
+++ pages/api/socket.js
@@ -0,0 +1,26 @@
+import { Server } from "socket.io";
+
+export default function handler(req, res) {
+    if (!res.socket.server.io) {
+        console.log("Starting socket.io server...");
+        const io = new Server(res.socket.server);
+        res.socket.server.io = io;
+
+        io.on("connection", (socket) => {
+            console.log("User connected", socket.id);
+
+            socket.on("addMessage", (elem) => {
+                socket.broadcast.emit("addMessage", elem);
+            });
+
+            socket.on("setDone", (elem) => {
+                socket.broadcast.emit("setDone", elem);
+            });
+
+            socket.on("disconnect", () => {
+                console.log("User disconnected", socket.id);
+            });
+        });
+    }
+    res.end();
+}
