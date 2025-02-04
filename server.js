84713c93986d7143acbce08bf87a15a2925e875a
MC <maciej.p@emiter.net.pl>
2025-02-04 13:25:17 +0100

    change port for vps

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 import socketHandler from './server/sockets/index.js';
 
 
-const port = parseInt(process.env.PORT || '3000', 10)
+const port = parseInt(process.env.PORT || '21541', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
